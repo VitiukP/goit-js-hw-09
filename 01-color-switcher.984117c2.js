cb6600cda1286a450e4d3fa86cd152e463b22f5f
VitiukP <107851187+VitiukP@users.noreply.github.com>
2023-07-27 13:12:53 +0000

    Deploying to gh-pages from @ VitiukP/goit-js-hw-09@cd9810a7fd963e4fbef78894f0eb32140a42c1bb 🚀

---
+++ 01-color-switcher.984117c2.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]"),e=null;t.addEventListener("click",(function(){null===e&&(e=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}),1e3))})),n.addEventListener("click",(function(){clearInterval(e),e=null}))}();
+//# sourceMappingURL=01-color-switcher.984117c2.js.map
---
+++ 01-color-switcher.ab4361ed.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let n=null;t.addEventListener("click",(function(){null===n&&(n=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}),1e3))})),e.addEventListener("click",(function(){clearInterval(n),n=null}));
+//# sourceMappingURL=01-color-switcher.ab4361ed.js.map
